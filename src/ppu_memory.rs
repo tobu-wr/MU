18a4e9679aa6f35a0c7a0b2aeec545a39b406094
Wilfried Rabouin <wilfried.rabouin@gmail.com>
2020-04-25 11:04:34 +0100

    Split ppu_memory into tables and palettes

--- src/ppu_memory.rs
+++ src/ppu_memory.rs
@@ -1,21 +1,47 @@
-const SIZE: usize = 0x4000;
+const TABLES_SIZE: usize = 0x3000;
+const TABLES_START: usize = 0x0000;
+const TABLES_END: usize = TABLES_START + TABLES_SIZE - 1;
+
+const PALETTES_SIZE: usize = 0x20;
+const PALETTES_START: usize = 0x3f00;
+const PALETTES_END: usize = PALETTES_START + PALETTES_SIZE - 1;
 
 pub struct PpuMemory {
-	data: [u8; SIZE]
+	tables: [u8; TABLES_SIZE],
+	palettes: [u8; PALETTES_SIZE]
 }
 
 impl PpuMemory {
 	pub fn new() -> Self {
 		Self {
-			data: [0; SIZE]
+			tables: [0; TABLES_SIZE],
+			palettes: [0; PALETTES_SIZE]
 		}
 	}
 
+	pub fn load_chr_rom(&mut self, chr_rom: &[u8]) {
+		self.tables[..chr_rom.len()].copy_from_slice(chr_rom);
+	}
+
 	pub fn read(&self, address: u16) -> u8 {
-		self.data[address as usize]
+		match address as usize {
+			TABLES_START ..= TABLES_END => self.tables[address as usize],
+			PALETTES_START ..= PALETTES_END => self.palettes[(address as usize) - PALETTES_START],
+			_ => {
+				println!("[ERROR] [PPU] Unhandled read from {:04X}", address);
+				std::process::exit(1);
+			}
+		}
 	}
 
 	pub fn write(&mut self, address: u16, value: u8) {
-		self.data[address as usize] = value;
+		match address as usize {
+			TABLES_START ..= TABLES_END => self.tables[address as usize] = value,
+			PALETTES_START ..= PALETTES_END => self.palettes[(address as usize) - PALETTES_START] = value,
+			_ => {
+				println!("[ERROR] [PPU] Unhandled write to {:04X}", address);
+				std::process::exit(1);
+			}
+		}
 	}
 }
