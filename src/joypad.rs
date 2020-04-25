011ba38f28078c7fe7ad770f1c13f8cb08fe42a8
Wilfried Rabouin <wilfried.rabouin@gmail.com>
2020-04-25 17:05:08 +0100

    Implement joypad controller

--- src/joypad.rs
+++ src/joypad.rs
@@ -1,13 +1,21 @@
-use minifb::Window;
+use minifb::{Key, Window};
+
+const BUTTON_COUNT: usize = 8;
 
 pub struct Joypad {
-    window: *const Window
+    window: *const Window,
+    buttons: [u8; BUTTON_COUNT],
+    polling: bool,
+    index: usize
 }
 
 impl Joypad {
     pub fn new() -> Self {
 		Self {
-			window: std::ptr::null()
+            window: std::ptr::null(),
+            buttons: [0; BUTTON_COUNT],
+            polling: false,
+            index: 0
 		}
     }
 
@@ -15,11 +23,40 @@ impl Joypad {
         self.window = window;
     }
 
-    pub fn read(&self) -> u8 {
-        0
+    fn is_key_down(&self, key: Key) -> u8 {
+        unsafe {
+            (*self.window).is_key_down(key)
+        }
+    }
+
+    pub fn read(&mut self) -> u8 {
+        if self.polling {
+            self.is_key_down(Key::A)
+        } else if self.index < BUTTON_COUNT {
+            let state = self.buttons[self.index];
+            self.index += 1;
+            state
+        } else {
+            1
+        }
     }
 
     pub fn write(&mut self, value: u8) {
-        // TODO
+        if (value & 1) == 1 {
+            self.polling = true;
+            self.index = 0;
+        } else {
+            unsafe {
+                self.buttons[0] = self.is_key_down(Key::A);
+                self.buttons[1] = self.is_key_down(Key::B);
+                self.buttons[2] = self.is_key_down(Key::Space);
+                self.buttons[3] = self.is_key_down(Key::Enter);
+                self.buttons[4] = self.is_key_down(Key::Up);
+                self.buttons[5] = self.is_key_down(Key::Down);
+                self.buttons[6] = self.is_key_down(Key::Left);
+                self.buttons[7] = self.is_key_down(Key::Right);
+            }
+            self.polling = false;
+        }
     }
 }
