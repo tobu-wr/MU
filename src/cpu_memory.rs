272a81e81b0845d1608d0f50ca81c8671b41a5f0
Wilfried Rabouin <wilfried.rabouin@gmail.com>
2020-04-28 13:40:59 +0100

    Clean up CpuMemory

--- src/cpu_memory.rs
+++ src/cpu_memory.rs
@@ -1,38 +1,31 @@
 use ppu::*;
 use joypad::*;
 
+pub const STACK_ADDRESS: u16 = 0x100;
 pub const NMI_VECTOR_ADDRESS: u16 = 0xfffa;
 pub const RESET_VECTOR_ADDRESS: u16 = 0xfffc;
 pub const IRQ_VECTOR_ADDRESS: u16 = 0xfffe;
 
-pub const STACK_ADDRESS: u16 = 0x100;
-
+const RAM_SIZE: u16 = 0x800;
 const RAM_START: u16 = 0;
 const RAM_END: u16 = 0x1fff;
-const RAM_SIZE: usize = 0x800;
-
-const PRG_RAM_START: u16 = 0x6000;
-const PRG_RAM_END: u16 = 0x7fff;
-
-const PRG_ROM_START: u16 = 0x8000;
-const PRG_ROM_END: u16 = 0xffff;
-
-const OAMDMA_ADDRESS: u16 = 0x4014;
-
-const APU_STATUS_ADDRESS: u16 = 0x4015;
-
-const JOY1_ADDRESS: u16 = 0x4016;
 
 const PPUCTRL_ADDRESS: u16 = 0x2000;
 const PPUMASK_ADDRESS: u16 = 0x2001;
 const PPUSTATUS_ADDRESS: u16 = 0x2002;
 const OAMADDR_ADDRESS: u16 = 0x2003;
+const OAMDATA_ADDRESS: u16 = 0x2004;
 const PPUSCROLL_ADDRESS: u16 = 0x2005;
 const PPUADDR_ADDRESS: u16 = 0x2006;
 const PPUDATA_ADDRESS: u16 = 0x2007;
+const OAMDMA_ADDRESS: u16 = 0x4014;
+const JOY1_ADDRESS: u16 = 0x4016;
+
+const PRG_ROM_START: u16 = 0x8000;
+const PRG_ROM_END: u16 = 0xffff;
 
 pub struct CpuMemory {
-	ram: [u8; RAM_SIZE],
+	ram: [u8; RAM_SIZE as _],
 	prg_rom: Vec<u8>,
 	ppu: *mut Ppu,
 	joypad: *mut Joypad
@@ -41,7 +34,7 @@ pub struct CpuMemory {
 impl CpuMemory {
 	pub fn new() -> Self {
 		Self {
-			ram: [0; RAM_SIZE],
+			ram: [0; RAM_SIZE as _],
 			prg_rom: Vec::new(),
 			ppu: std::ptr::null_mut(),
 			joypad: std::ptr::null_mut()
@@ -63,40 +56,36 @@ impl CpuMemory {
 		}
 	}
 
+	#[cfg(feature = "log")]
+	fn read_ppu_debug(&self, register: Register) -> u8 {
+		unsafe {
+			(*self.ppu).read_register_debug(register)
+		}
+	}
+
+	fn write_ppu(&self, register: Register, value: u8) {
+		unsafe {
+			(*self.ppu).write_register(register, value);
+		}
+	}
+
 	pub fn read8(&self, address: u16) -> u8 {
 		match address {
-			RAM_START ..= RAM_END => {
-				let effective_address = address as usize % RAM_SIZE;
-				self.ram[effective_address]
-			},
+			RAM_START ..= RAM_END => self.ram[(address % RAM_SIZE) as usize],
 			PPUSTATUS_ADDRESS => self.read_ppu(Register::Ppustatus),
 			PPUDATA_ADDRESS => self.read_ppu(Register::Ppudata),
-			0x4000 | 0x4001 | 0x4002 | 0x4003 | 0x4004 | 0x4005 | 0x4006 | 0x4007 | 0x4008 | 0x4009 | 0x400a | 0x400b | 0x400c | 0x400d | 0x400e | 0x400f | 0x4010 | 0x4011 | 0x4012 | 0x4013 | 0x4017 | APU_STATUS_ADDRESS => {
+			0x4000 ..= 0x4013 | 0x4015 | 0x4017 => {
 				println!("[DEBUG] [CPU] Read from an APU register");
 				0
 			},
 			JOY1_ADDRESS => unsafe {
 				(*self.joypad).read()
 			},
-			PRG_ROM_START ..= PRG_ROM_END => {
-				let mut effective_address = (address - PRG_ROM_START) as usize;
-				if effective_address >= self.prg_rom.len() {
-					effective_address -= self.prg_rom.len();
-				}
-
-				self.prg_rom[effective_address]
-			},
+			PRG_ROM_START ..= PRG_ROM_END => self.prg_rom[((address - PRG_ROM_START) as usize) % self.prg_rom.len()],
 			_ => {
-				println!("[ERROR] [CPU] Unhandled read8 from {:04X}", address);
+				println!("[ERROR] [CPU] Unhandled read from {:04X}", address);
 				std::process::exit(1);
 			}
-		}
-	}
-
-	#[cfg(feature = "log")]
-	fn read_ppu_debug(&self, register: Register) -> u8 {
-		unsafe {
-			(*self.ppu).read_register_debug(register)
 		}
 	}
 
@@ -114,57 +103,24 @@ impl CpuMemory {
 	}
 
 	pub fn read16(&self, address: u16) -> u16 {
-		match address {
-			RAM_START ..= RAM_END => {
-				let effective_address = address as usize % RAM_SIZE;
-				let low_byte = self.ram[effective_address] as u16;
-				let high_byte = self.ram[effective_address.wrapping_add(1)] as u16;
-				(high_byte << 8) | low_byte
-			},
-			PRG_ROM_START ..= PRG_ROM_END => {
-				let mut effective_address = (address - PRG_ROM_START) as usize;
-				if effective_address >= self.prg_rom.len() {
-					effective_address -= self.prg_rom.len();
-				}
-
-				let low_byte = self.prg_rom[effective_address] as u16;
-				let high_byte = self.prg_rom[effective_address.wrapping_add(1)] as u16;
-				(high_byte << 8) | low_byte
-			},
-			_ => {
-				println!("[ERROR] [CPU] Unhandled read16 from {:04X}", address);
-				std::process::exit(1);
-			}
-		}
-	}
-
-	fn write_ppu(&self, register: Register, value: u8) {
-		unsafe {
-			(*self.ppu).write_register(register, value);
-		}
+		let low_byte = self.read8(address) as u16;
+		let high_byte = self.read8(address.wrapping_add(1)) as u16;
+		(high_byte << 8) | low_byte
 	}
 
 	pub fn write8(&mut self, address: u16, value: u8) {
 		match address {
-			RAM_START ..= RAM_END => {
-				let effective_address = address as usize % RAM_SIZE;
-				self.ram[effective_address] = value;
-			},
-			PRG_RAM_START ..= PRG_RAM_END => {
-				println!("[DEBUG] [CPU] Write to PRG RAM {:02X}", value);
-			},
+			RAM_START ..= RAM_END => self.ram[(address % RAM_SIZE) as usize] = value,
 			PPUCTRL_ADDRESS => self.write_ppu(Register::Ppuctrl, value),
 			PPUMASK_ADDRESS => self.write_ppu(Register::Ppumask, value),
 			OAMADDR_ADDRESS => self.write_ppu(Register::Oamaddr, value),
-			0x2004 => {
+			OAMDATA_ADDRESS => {
 				println!("[DEBUG] [CPU] Write to OAMDATA {:02X}", value);
 			},
 			PPUSCROLL_ADDRESS => self.write_ppu(Register::Ppuscroll, value),
 			PPUADDR_ADDRESS => self.write_ppu(Register::Ppuaddr, value),
 			PPUDATA_ADDRESS => self.write_ppu(Register::Ppudata, value),
-			0x4000 | 0x4001 | 0x4002 | 0x4003 | 0x4004 | 0x4005 | 0x4006 | 0x4007 | 0x4008 | 0x4009 | 0x400a | 0x400b | 0x400c | 0x400d | 0x400e | 0x400f | 0x4010 | 0x4011 | 0x4012 | 0x4013 | 0x4017 | APU_STATUS_ADDRESS => {
-				println!("[DEBUG] [CPU] Write to an APU register");
-			},
+			0x4000 ..= 0x4013 | 0x4015 | 0x4017 => println!("[DEBUG] [CPU] Write to an APU register"),
 			OAMDMA_ADDRESS => {
 				let start_address = (value as usize) << 8;
 				let end_address = start_address + OAM_SIZE;
