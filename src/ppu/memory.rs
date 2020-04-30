7b8a25912504a669fd8b98d454660929ef12d202
Wilfried Rabouin <wilfried.rabouin@gmail.com>
2020-04-30 18:49:55 +0100

    enforce privacy of ppu memory members

--- src/ppu/memory.rs
+++ src/ppu/memory.rs
@@ -10,20 +10,20 @@ const PALETTES_SIZE: u16 = 0x20;
 const PALETTES_START: u16 = 0x3f00;
 const PALETTES_END: u16 = 0x3fff;
 
-pub struct Memory {
+pub(super) struct Memory {
 	tables: [u8; TABLES_SIZE as _],
 	palettes: [u8; PALETTES_SIZE as _]
 }
 
 impl Memory {
-	pub fn new() -> Self {
+	pub(super) fn new() -> Self {
 		Self {
 			tables: [0; TABLES_SIZE as _],
 			palettes: [0; PALETTES_SIZE as _]
 		}
 	}
 
-	pub fn load_chr_rom(&mut self, chr_rom: &[u8]) {
+	pub(super) fn load_chr_rom(&mut self, chr_rom: &[u8]) {
 		self.tables[..chr_rom.len()].copy_from_slice(chr_rom);
 	}
 
