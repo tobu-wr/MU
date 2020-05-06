619caea2c3bf9cdabcfd1d6efc42caf33be86375
Wilfried Rabouin <wilfried.rabouin@gmail.com>
2020-05-06 23:37:53 +0100

    Fix cpu test path

--- src/cpu/tests.rs
+++ src/cpu/tests.rs
@@ -3,7 +3,7 @@ use super::*;
 #[test]
 fn nestest() {
 	let mut emulator = Emulator::new();
-	emulator.load_file("tests/nestest.nes");
+	emulator.load_file("tests/cpu/nestest.nes");
 	emulator.cpu.set_pc(0xc000);
 	for _ in 0..8991 {
 		Cpu::execute_next_instruction(&mut emulator);
@@ -27,82 +27,82 @@ fn run_test(filename: &str) {
 
 #[test]
 fn basics() {
-	run_test("tests/01-basics.nes");
+	run_test("tests/cpu/01-basics.nes");
 }
 
 #[test]
 fn implied() {
-	run_test("tests/02-implied.nes");
+	run_test("tests/cpu/02-implied.nes");
 }
 
 #[test]
 fn immediate() {
-	run_test("tests/03-immediate.nes");
+	run_test("tests/cpu/03-immediate.nes");
 }
 
 #[test]
 fn zero_page() {
-	run_test("tests/04-zero_page.nes");
+	run_test("tests/cpu/04-zero_page.nes");
 }
 
 #[test]
 fn zp_xy() {
-	run_test("tests/05-zp_xy.nes");
+	run_test("tests/cpu/05-zp_xy.nes");
 }
 
 #[test]
 fn absolute() {
-	run_test("tests/06-absolute.nes");
+	run_test("tests/cpu/06-absolute.nes");
 }
 
 #[test]
 #[ignore] // FIXME
 fn abs_xy() {
-	run_test("tests/07-abs_xy.nes");
+	run_test("tests/cpu/07-abs_xy.nes");
 }
 
 #[test]
 fn ind_x() {
-	run_test("tests/08-ind_x.nes");
+	run_test("tests/cpu/08-ind_x.nes");
 }
 
 #[test]
 fn ind_y() {
-	run_test("tests/09-ind_y.nes");
+	run_test("tests/cpu/09-ind_y.nes");
 }
 
 #[test]
 fn branches() {
-	run_test("tests/10-branches.nes");
+	run_test("tests/cpu/10-branches.nes");
 }
 
 #[test]
 #[ignore] // FIXME
 fn stack() {
-	run_test("tests/11-stack.nes");
+	run_test("tests/cpu/11-stack.nes");
 }
 
 #[test]
 fn jmp_jsr() {
-	run_test("tests/12-jmp_jsr.nes");
+	run_test("tests/cpu/12-jmp_jsr.nes");
 }
 
 #[test]
 fn rts() {
-	run_test("tests/13-rts.nes");
+	run_test("tests/cpu/13-rts.nes");
 }
 
 #[test]
 fn rti() {
-	run_test("tests/14-rti.nes");
+	run_test("tests/cpu/14-rti.nes");
 }
 
 #[test]
 fn brk() {
-	run_test("tests/15-brk.nes");
+	run_test("tests/cpu/15-brk.nes");
 }
 
 #[test]
 fn special() {
-	run_test("tests/16-special.nes");
+	run_test("tests/cpu/16-special.nes");
 }
