f22fa5bda124919f3973ab1469b0752368d4eff7
Wilfried Rabouin <wilfried.rabouin@gmail.com>
2020-05-06 22:40:51 +0100

    rewrite ppu memory map

--- src/ppu/memory.rs
+++ src/ppu/memory.rs
@@ -1,37 +1,80 @@
-const TABLES_SIZE: u16 = 0x3000;
-const TABLES_START: u16 = 0x0000;
-const TABLES_END: u16 = TABLES_START + TABLES_SIZE - 1;
+const TABLE_SIZE: u16 = 0x3000;
+const TABLE_START: u16 = 0x0000;
+const TABLE_END: u16 = TABLE_START + TABLE_SIZE - 1;
 
-const TABLES_MIRRORS_OFFSET: u16 = 0x1000;
-const TABLES_MIRRORS_START: u16 = 0x3000;
-const TABLES_MIRRORS_END: u16 = 0x3eff;
+const TABLE_MIRROR_OFFSET: u16 = 0x1000;
+const TABLE_MIRROR_START: u16 = 0x3000;
+const TABLE_MIRROR_END: u16 = 0x3eff;
 
-const PALETTES_SIZE: u16 = 0x20;
-const PALETTES_START: u16 = 0x3f00;
-const PALETTES_END: u16 = 0x3fff;
+const PALETTE_SIZE: u16 = 0x20;
+const PALETTE_START: u16 = 0x3f00;
+const PALETTE_END: u16 = 0x3fff;
+
+const BACKGROUND_PALETTE_SIZE: u16 = 0x10;
+const BACKGROUND_PALETTE_START: u16 = 0x3f00;
+const BACKGROUND_PALETTE_END: u16 = 0x3f0f;
+
+const BACKGROUND_PALETTE_MIRROR_ADDRESS_0: u16 = 0x3f10;
+const BACKGROUND_PALETTE_MIRROR_ADDRESS_1: u16 = 0x3f14;
+const BACKGROUND_PALETTE_MIRROR_ADDRESS_2: u16 = 0x3f18;
+const BACKGROUND_PALETTE_MIRROR_ADDRESS_3: u16 = 0x3f1c;
+
+const SPRITE_PALETTE_SIZE: usize = 12;
+
+const SPRITE_PALETTE_START_0: u16 = 0x3f11;
+const SPRITE_PALETTE_END_0: u16 = 0x3f13;
+
+const SPRITE_PALETTE_START_1: u16 = 0x3f15;
+const SPRITE_PALETTE_END_1: u16 = 0x3f17;
+
+const SPRITE_PALETTE_START_2: u16 = 0x3f19;
+const SPRITE_PALETTE_END_2: u16 = 0x3f1b;
+
+const SPRITE_PALETTE_START_3: u16 = 0x3f1d;
+const SPRITE_PALETTE_END_3: u16 = 0x3f1f;
 
 pub(super) struct Memory {
-	tables: [u8; TABLES_SIZE as _],
-	palettes: [u8; PALETTES_SIZE as _]
+	table: [u8; TABLE_SIZE as _],
+	background_palette: [u8; BACKGROUND_PALETTE_SIZE as _],
+	sprite_palette: [u8; SPRITE_PALETTE_SIZE]
 }
 
 impl Memory {
 	pub(super) fn new() -> Self {
 		Self {
-			tables: [0; TABLES_SIZE as _],
-			palettes: [0; PALETTES_SIZE as _]
+			table: [0; TABLE_SIZE as _],
+			background_palette: [0; BACKGROUND_PALETTE_SIZE as _],
+			sprite_palette: [0; SPRITE_PALETTE_SIZE]
 		}
 	}
 
 	pub(super) fn load_chr_rom(&mut self, chr_rom: &[u8]) {
-		self.tables[..chr_rom.len()].copy_from_slice(chr_rom);
+		self.table[..chr_rom.len()].copy_from_slice(chr_rom);
 	}
 
 	pub(super) fn read(&self, address: u16) -> u8 {
 		match address {
-			TABLES_START ..= TABLES_END => self.tables[address as usize],
-			TABLES_MIRRORS_START ..= TABLES_MIRRORS_END => self.tables[(address - TABLES_MIRRORS_OFFSET) as usize],
-			PALETTES_START ..= PALETTES_END => self.palettes[((address - PALETTES_START) % PALETTES_SIZE) as usize],
+			TABLE_START ..= TABLE_END => self.table[(address - TABLE_START) as usize],
+			TABLE_MIRROR_START ..= TABLE_MIRROR_END => self.table[(address - TABLE_MIRROR_OFFSET) as usize],
+			PALETTE_START ..= PALETTE_END => match PALETTE_START + address % PALETTE_SIZE {
+				BACKGROUND_PALETTE_START ..= BACKGROUND_PALETTE_END => self.background_palette[(address - BACKGROUND_PALETTE_START) as usize],
+				BACKGROUND_PALETTE_MIRROR_ADDRESS_0 => self.background_palette[0],
+				BACKGROUND_PALETTE_MIRROR_ADDRESS_1 => self.background_palette[4],
+				BACKGROUND_PALETTE_MIRROR_ADDRESS_2 => self.background_palette[8],
+				BACKGROUND_PALETTE_MIRROR_ADDRESS_3 => self.background_palette[12],
+				SPRITE_PALETTE_START_0 ..= SPRITE_PALETTE_END_0 |
+				SPRITE_PALETTE_START_1 ..= SPRITE_PALETTE_END_1 |
+				SPRITE_PALETTE_START_2 ..= SPRITE_PALETTE_END_2 |
+				SPRITE_PALETTE_START_3 ..= SPRITE_PALETTE_END_3 => {
+					let palette = (address >> 2) & 0b11;
+					let index = address & 0b11;
+					self.sprite_palette[(palette * 3 + index - 1) as usize]
+				},
+				_ => {
+					error!("Read from {:04X}", address);
+					panic!();
+				}
+			},
 			_ => {
 				error!("Read from {:04X}", address);
 				panic!();
@@ -41,9 +84,27 @@ impl Memory {
 
 	pub(super) fn write(&mut self, address: u16, value: u8) {
 		match address {
-			TABLES_START ..= TABLES_END => self.tables[address as usize] = value,
-			TABLES_MIRRORS_START ..= TABLES_MIRRORS_END => self.tables[(address - TABLES_MIRRORS_OFFSET) as usize] = value,
-			PALETTES_START ..= PALETTES_END => self.palettes[((address - PALETTES_START) % PALETTES_SIZE) as usize] = value,
+			TABLE_START ..= TABLE_END => self.table[(address - TABLE_START) as usize] = value,
+			TABLE_MIRROR_START ..= TABLE_MIRROR_END => self.table[(address - TABLE_MIRROR_OFFSET) as usize] = value,
+			PALETTE_START ..= PALETTE_END => match PALETTE_START + address % PALETTE_SIZE {
+				BACKGROUND_PALETTE_START ..= BACKGROUND_PALETTE_END => self.background_palette[(address - BACKGROUND_PALETTE_START) as usize] = value,
+				BACKGROUND_PALETTE_MIRROR_ADDRESS_0 => self.background_palette[0] = value,
+				BACKGROUND_PALETTE_MIRROR_ADDRESS_1 => self.background_palette[4] = value,
+				BACKGROUND_PALETTE_MIRROR_ADDRESS_2 => self.background_palette[8] = value,
+				BACKGROUND_PALETTE_MIRROR_ADDRESS_3 => self.background_palette[12] = value,
+				SPRITE_PALETTE_START_0 ..= SPRITE_PALETTE_END_0 |
+				SPRITE_PALETTE_START_1 ..= SPRITE_PALETTE_END_1 |
+				SPRITE_PALETTE_START_2 ..= SPRITE_PALETTE_END_2 |
+				SPRITE_PALETTE_START_3 ..= SPRITE_PALETTE_END_3 => {
+					let palette = (address >> 2) & 0b11;
+					let index = address & 0b11;
+					self.sprite_palette[(palette * 3 + index - 1) as usize]  = value;
+				},
+				_ => {
+					error!("Write to {:04X}", address);
+					panic!();
+				}
+			},
 			_ => {
 				error!("Write to {:04X}", address);
 				panic!();
