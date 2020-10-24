c832042fe195b66e8e1e6e8c56f97b0b4fe0aefd
Wilfried Rabouin <wilfried.rabouin@gmail.com>
2020-10-24 19:12:10 +0100

    implement nrom mapper

--- src/mappers/nrom.rs
+++ src/mappers/nrom.rs
@@ -1,17 +1,40 @@
 use super::*;
 
+const PRG_RAM_SIZE: usize = 0x2000;
+const PRG_RAM_START: u16 = 0x6000;
+const PRG_RAM_END: u16 = 0x7fff;
+
+const PRG_ROM_START: u16 = 0x8000;
+const PRG_ROM_END: u16 = 0xffff;
+
 pub(super) struct Nrom {
-    // TODO
+    prg_ram: [u8; PRG_RAM_SIZE],
+    prg_rom: Vec<u8>
 }
 
 impl Nrom {
-    pub(super) fn new() -> Self {
+    pub(super) fn new(prg_rom: &[u8]) -> Self {
         Self {
-            // TODO
+            prg_ram: [0; PRG_RAM_SIZE],
+            prg_rom: prg_rom.to_vec()
         }
     }
 }
 
 impl Mapper for Nrom {
-    // TODO
+    fn read(&self, address: u16) -> u8 {
+        match address {
+            PRG_RAM_START ..= PRG_RAM_END => self.prg_ram[(address - PRG_RAM_START) as usize],
+            PRG_ROM_START ..= PRG_ROM_END => self.prg_rom[((address - PRG_ROM_START) as usize) % self.prg_rom.len()],
+            _ => unimplemented!()
+        }
+    }
+
+    fn write(&mut self, address: u16, value: u8) {
+        match address {
+            PRG_RAM_START ..= PRG_RAM_END => self.prg_ram[(address - PRG_RAM_START) as usize] = value,
+            PRG_ROM_START ..= PRG_ROM_END => {}, // ignore writes to PRG ROM
+            _ => unimplemented!()
+        }
+    }
 }
