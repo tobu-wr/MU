a9afc2dda2f64cf1f4b9ae5526bb5af751b9fbc6
Wilfried Rabouin <wilfried.rabouin@gmail.com>
2020-10-27 22:01:57 +0100

    UxROM: review

--- src/mappers/uxrom.rs
+++ src/mappers/uxrom.rs
@@ -2,22 +2,22 @@ use super::*;
 
 const PRG_ROM_BANK_SIZE: usize = 0x4000;
 
-const PRG_ROM_SWITCHABLE_BANK_START: u16 = 0x8000;
-const PRG_ROM_SWITCHABLE_BANK_END: u16 = 0xbfff;
+const PRG_ROM_BANK_0_START: u16 = 0x8000;
+const PRG_ROM_BANK_0_END: u16 = 0xbfff;
 
-const PRG_ROM_LAST_BANK_START: u16 = 0xc000;
-const PRG_ROM_LAST_BANK_END: u16 = 0xffff;
+const PRG_ROM_BANK_1_START: u16 = 0xc000;
+const PRG_ROM_BANK_1_END: u16 = 0xffff;
 
 pub(super) struct Uxrom {
     prg_rom: Vec<u8>,
-    bank: u8
+    prg_rom_bank: u8
 }
 
 impl Uxrom {
     pub(super) fn new(prg_rom: &[u8]) -> Self {
         Self {
             prg_rom: prg_rom.to_vec(),
-            bank: 0
+            prg_rom_bank: 0
         }
     }
 }
@@ -26,15 +26,15 @@ impl Mapper for Uxrom {
     fn read(&self, address: u16) -> u8 {
         match address {
             0x6000 ..= 0x7fff => 0,
-            PRG_ROM_SWITCHABLE_BANK_START ..= PRG_ROM_SWITCHABLE_BANK_END => self.prg_rom[(address - PRG_ROM_SWITCHABLE_BANK_START) as usize + PRG_ROM_BANK_SIZE * self.bank as usize],
-            PRG_ROM_LAST_BANK_START ..= PRG_ROM_LAST_BANK_END => self.prg_rom[(address - PRG_ROM_LAST_BANK_START) as usize + self.prg_rom.len() - PRG_ROM_BANK_SIZE],
+            PRG_ROM_BANK_0_START ..= PRG_ROM_BANK_0_END => self.prg_rom[(address - PRG_ROM_BANK_0_START) as usize + PRG_ROM_BANK_SIZE * self.prg_rom_bank as usize], // switchable
+            PRG_ROM_BANK_1_START ..= PRG_ROM_BANK_1_END => self.prg_rom[(address - PRG_ROM_BANK_1_START) as usize + self.prg_rom.len() - PRG_ROM_BANK_SIZE], // fixed to last bank
             _ => unimplemented!()
         }
     }
 
     fn write(&mut self, address: u16, value: u8) {
         match address {
-            0x8000 ..= 0xffff => self.bank = value & 0b1111,
+            0x8000 ..= 0xffff => self.prg_rom_bank = value & 0b1111,
             _ => unimplemented!()
         }
     }
