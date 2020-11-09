cd6f7adeb4a36f42d491b6d857525ff75710848e
Wilfried Rabouin <wilfried.rabouin@gmail.com>
2020-11-09 20:34:27 +0000

    review joypad

--- src/joypad.rs
+++ src/joypad.rs
@@ -1,21 +1,99 @@
-use window::*;
-
 pub struct Joypad {
     register: u8,
-    polling: bool
+    strobe: bool,
+    a_button_down: bool,
+    b_button_down: bool,
+    select_button_down: bool,
+    start_button_down: bool,
+    up_button_down: bool,
+    down_button_down: bool,
+    left_button_down: bool,
+    right_button_down: bool
 }
 
 impl Joypad {
     pub fn new() -> Self {
         Self {
             register: 0,
-            polling: false
+            strobe: false,
+            a_button_down: false,
+            b_button_down: false,
+            select_button_down: false,
+            start_button_down: false,
+            up_button_down: false,
+            down_button_down: false,
+            left_button_down: false,
+            right_button_down: false
         }
     }
 
+    pub fn press_a_button(&mut self) {
+        self.a_button_down = true;
+    }
+
+    pub fn press_b_button(&mut self) {
+        self.b_button_down = true;
+    }
+
+    pub fn press_select_button(&mut self) {
+        self.select_button_down = true;
+    }
+
+    pub fn press_start_button(&mut self) {
+        self.start_button_down = true;
+    }
+
+    pub fn press_up_button(&mut self) {
+        self.up_button_down = true;
+    }
+
+    pub fn press_down_button(&mut self) {
+        self.down_button_down = true;
+    }
+
+    pub fn press_left_button(&mut self) {
+        self.left_button_down = true;
+    }
+
+    pub fn press_right_button(&mut self) {
+        self.right_button_down = true;
+    }
+
+    pub fn release_a_button(&mut self) {
+        self.a_button_down = false;
+    }
+
+    pub fn release_b_button(&mut self) {
+        self.b_button_down = false;
+    }
+
+    pub fn release_select_button(&mut self) {
+        self.select_button_down = false;
+    }
+
+    pub fn release_start_button(&mut self) {
+        self.start_button_down = false;
+    }
+
+    pub fn release_up_button(&mut self) {
+        self.up_button_down = false;
+    }
+
+    pub fn release_down_button(&mut self) {
+        self.down_button_down = false;
+    }
+
+    pub fn release_left_button(&mut self) {
+        self.left_button_down = false;
+    }
+
+    pub fn release_right_button(&mut self) {
+        self.right_button_down = false;
+    }
+
     pub fn read(&mut self, window: &Window) -> u8 {
-        if self.polling {
-            window.is_key_down(Key::Q) as u8
+        if self.strobe {
+            a_button_down as u8
         } else {
             let state = self.register & 1;
             self.register >>= 1;
@@ -25,8 +103,8 @@ impl Joypad {
 
     #[cfg(any(feature = "trace", test))]
     pub fn read_debug(&self, window: &Window) -> u8 {
-        if self.polling {
-            window.is_key_down(Key::Q) as u8
+        if self.strobe {
+            a_button_down as u8
         } else {
             self.register & 1
         }
@@ -34,17 +112,17 @@ impl Joypad {
 
     pub fn write(&mut self, window: &Window, value: u8) {
         if (value & 1) == 1 {
-            self.polling = true;
-        } else if self.polling {
-            self.polling = false;
-            self.register = (window.is_key_down(Key::Q) as u8) << 0
-                          | (window.is_key_down(Key::W) as u8) << 1
-                          | (window.is_key_down(Key::Space) as u8) << 2
-                          | (window.is_key_down(Key::Enter) as u8) << 3
-                          | (window.is_key_down(Key::Up) as u8) << 4
-                          | (window.is_key_down(Key::Down) as u8) << 5
-                          | (window.is_key_down(Key::Left) as u8) << 6
-                          | (window.is_key_down(Key::Right) as u8) << 7;
+            self.strobe = true;
+        } else if self.strobe {
+            self.strobe = false;
+            self.register = (a_button_down as u8) << 0
+                          | (b_button_down as u8) << 1
+                          | (select_button_down as u8) << 2
+                          | (start_button_down as u8) << 3
+                          | (up_button_down as u8) << 4
+                          | (down_button_down as u8) << 5
+                          | (left_button_down as u8) << 6
+                          | (right_button_down as u8) << 7;
         }
     }
 }
