01d5ab1e3040faad738e30eccbc096ba664b7e3c
Wilfried Rabouin <wilfried.rabouin@gmail.com>
2020-12-28 22:00:04 +0000

    haha

--- src/apu/mod.rs
+++ src/apu/mod.rs
@@ -1,11 +1,15 @@
 pub struct Apu {
-
+    // TODO
 }
 
 impl Apu {
     pub fn new() -> Self {
         Self {
-            
+           // TODO 
         }
+    }
+
+    pub fn do_cycle(&mut self) {
+        // TODO
     }
 }
